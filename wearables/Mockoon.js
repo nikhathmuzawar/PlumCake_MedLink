ad7d79ea28c3862640c7ed876ffdbb749c4bddca
Srushtei <123564247+Srushtei@users.noreply.github.com>
2024-12-22 00:31:05 +0530

    Mockoon.js

--- wearables/Mockoon.js
+++ wearables/Mockoon.js
@@ -3,8 +3,8 @@
     {
       "id": "{{faker 'string.uuid'}}",
       "name": "{{faker 'person.firstName'}} {{faker 'person.lastName'}}",
+      "email": "{{faker 'internet.email'}}",
       "phone": "{{faker 'phone.number'}}",
-      "address": "{{faker 'location.streetAddress'}}, {{faker 'location.city'}}, {{faker 'location.state' abbreviated=true}} {{faker 'location.zipCode'}}",
       "timestamp": "{{faker 'date.recent'}}",
       "bloodPressure": {
         "systolic": "{{faker 'number.int' min=90 max=180}}",
@@ -13,8 +13,9 @@
       "heartRate": "{{faker 'number.int' min=60 max=100}}",
       "stressLevel": "{{faker 'number.int' min=1 max=10}}",
       "physicalActivity": {
+        "activity": "{{faker 'helpers.arrayElement' array=['Walking', 'Running', 'Swimming', 'Cycling', 'Yoga']}}",
+        "duration": "{{faker 'number.int' min=10 max=120}} minutes",
         "steps": "{{faker 'number.int' min=0 max=15000}}",
-        "activeMinutes": "{{faker 'number.int' min=0 max=300}}",
         "caloriesBurned": "{{faker 'number.int' min=0 max=3000}}"
       },
       "oxygenLevel": "{{faker 'number.int' min=90 max=100}}"
